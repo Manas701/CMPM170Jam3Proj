4be1772e2c47e3178b1c73af91e55ecd035f6f5b
Menace791 <manas.s.sara@gmail.com>
2022-11-22 11:48:23 -0800

    nvm youtube tutorials are op

--- CMPM170Jam3/Assets/FollowPlayers.cs
+++ CMPM170Jam3/Assets/FollowPlayers.cs
@@ -2,25 +2,76 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class FollowPlayers : MonoBehaviour
 {
-    private Transform p1;
-    private Transform p2;
-    private float camX;
-    private float camY;
+    
+    public List<Transform> players;
 
-    // Start is called before the first frame update
+    public Vector3 offset;
+    public float smoothTime;
+
+    public float minZoom = 40f;
+    public float maxZoom = 10f;
+    public float zoomLimiter = 50f;
+
+    private Vector3 velocity;
+    private Camera cam;
+
     void Start()
     {
-        p1 = GameObject.Find("Player1").transform;
-        p2 = GameObject.Find("Player2").transform;
+        cam = GetComponent<Camera>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        camX = (p1.position.x + p2.position.x) / 2;
-        camY = (p1.position.y + p2.position.y) / 2;
-        transform.position = new Vector3(camX, camY, transform.position.z);
+        if (players.Count == 0)
+            return;
+
+        Move();
+        Zoom();
     }
+
+    void Move()
+    {
+        Vector3 centerPoint = GetCenterPoint();
+
+        Vector3 newPos = centerPoint + offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, newPos, ref velocity, smoothTime);
+    }
+
+    void Zoom()
+    {
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimiter);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
+    }
+
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(players[0].position, Vector3.zero);
+        for (int i = 0; i < players.Count; i++)
+        {
+            bounds.Encapsulate(players[i].position);
+        }
+
+        return bounds.size.x;
+    }
+
+    Vector3 GetCenterPoint()
+    {
+        if (players.Count == 1)
+        {
+            return players[0].position;
+        }
+
+        var bounds = new Bounds(players[0].position, Vector3.zero);
+        for (int i = 0; i < players.Count; i++)
+        {
+            bounds.Encapsulate(players[i].position);
+        }
+
+        return bounds.center;
+    }
+
 }
